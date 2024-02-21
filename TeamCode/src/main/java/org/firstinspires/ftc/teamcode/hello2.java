5915485a8cfb8cd98dbbca96716bb7cd89bc5e6c
Kevin <meowmeowahr@gmail.com>
2024-02-20 20:58:19 -0600

    stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hello2.java
@@ -0,0 +1,118 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+@TeleOp (name = "NewCompCode" ,group = "Linear Opmode")
+
+//@Disabled
+public class hello2 extends LinearOpMode {
+    private static final double NORMAL_SPEED = 1;
+    private static final double SLOW_SPEED = 0.40;
+
+    private IMU imu = null; // Control/Expansion Hub IMU
+
+    //Motor demo variables
+    private DcMotorEx frontLeftDrive = null;
+    private DcMotorEx frontRightDrive = null;
+    private DcMotorEx backLeftDrive = null;
+    private DcMotorEx backRightDrive = null;
+
+
+    private void initializeMotors()
+    {
+        // Drive
+        frontLeftDrive = hardwareMap.get(DcMotorEx.class, "FL");
+        frontRightDrive = hardwareMap.get(DcMotorEx.class, "FR");
+        backLeftDrive = hardwareMap.get(DcMotorEx.class, "BL");
+        backRightDrive = hardwareMap.get(DcMotorEx.class, "BR");
+
+        frontRightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        backLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        frontLeftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+    }
+
+    private void initializeImu() {
+        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
+        RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
+
+        // Now initialize the IMU with this mounting orientation
+        // This sample expects the IMU to be in a REV Hub and named "imu".
+        imu = hardwareMap.get(IMU.class, "imu");
+        imu.initialize(new IMU.Parameters(orientationOnRobot));
+    }
+
+    private void joystickMecanumDrive() {
+
+        double y = 0;
+        double x = 0;
+        double rx = 0;
+
+        if (!gamepad1.right_bumper) {
+             y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+             x = gamepad1.left_stick_x;
+             rx = gamepad1.right_stick_x;
+        }
+
+        double speed_multiplier;
+        if (gamepad1.left_trigger > 0.5) {
+            speed_multiplier = SLOW_SPEED;
+        } else {
+            speed_multiplier = NORMAL_SPEED;
+        }
+
+
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio,
+        // but only if at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(y) + Math.abs(rx) + Math.abs(x), 1);
+        double frontLeftPower = -(y + x + rx) / denominator; //A-RC has negative before parentheses and B-RC has no negative
+        double backLeftPower = -(y - x + rx) / denominator;
+        double frontRightPower = -(y - x - rx) / denominator;
+        double backRightPower = -(y + x - rx) / denominator;
+
+        frontLeftDrive.setPower(frontLeftPower * speed_multiplier);
+        backLeftDrive.setPower(backLeftPower * speed_multiplier);
+        frontRightDrive.setPower(frontRightPower * speed_multiplier);
+        backRightDrive.setPower(backRightPower * speed_multiplier);
+
+        telemetry.addData("backRightPower", backRightPower);
+        telemetry.addData("backLeftPower", backLeftPower);
+        telemetry.addData("frontRightPower", frontRightPower);
+        telemetry.addData("frontLeftPower", frontLeftPower);
+
+    }
+
+    public void runOpMode()  {
+        initializeMotors();
+        initializeImu();
+        waitForStart();
+        imu.resetYaw();
+        while (opModeIsActive()) {
+            joystickMecanumDrive();
+            //fieldCentricDrive();
+
+            updateTelemetry(telemetry);
+        }
+    }
+}
