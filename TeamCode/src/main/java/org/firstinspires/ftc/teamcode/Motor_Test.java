a972904175568fbcf8e034d97f811062ec04949f
Kevin Ahr <meowmeowahr@gmail.com>
2024-02-24 08:03:23 -0600

    add encoder test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motor_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motor_Test.java
@@ -1,15 +1,9 @@
 package org.firstinspires.ftc.teamcode;
-
-import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp (name = "TEST_MotorEncoders" ,group = "Test")
 
