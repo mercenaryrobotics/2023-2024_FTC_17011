836d194b25afe41e3982c5491ae513b3919472f6
Kevin Ahr <meowmeowahr@gmail.com>
2024-02-24 00:02:06 -0600

    add encoder test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motor_Test.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp (name = "TEST_MotorEncoders" ,group = "Test")
+
+@Disabled
+public class Motor_Test extends LinearOpMode {
+    //Motor demo variables
+    private DcMotorEx frontLeftDrive = null;
+    private DcMotorEx frontRigtDrive = null;
+    private DcMotorEx backLeftDrive = null;
+    private DcMotorEx backRightDrive = null;
+
+    public void initializeMotors()
+    {
+        frontLeftDrive = hardwareMap.get(DcMotorEx.class, "FL");
+        frontRigtDrive = hardwareMap.get(DcMotorEx.class, "FR");
+        backLeftDrive = hardwareMap.get(DcMotorEx.class, "BL");
+        backRightDrive = hardwareMap.get(DcMotorEx.class, "BR");
+
+        frontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRigtDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void runOpMode()  {
+        initializeMotors();
+
+        waitForStart();
+        while (opModeIsActive()) {
+            telemetry.addData("FL (0)", frontLeftDrive.getCurrentPosition());
+            telemetry.addData("FR (1)", frontRigtDrive.getCurrentPosition());
+            telemetry.addData("BL (2)", backLeftDrive.getCurrentPosition());
+            telemetry.addData("BR (3)", backRightDrive.getCurrentPosition());
+            updateTelemetry(telemetry);
+        }
+    }
+}
